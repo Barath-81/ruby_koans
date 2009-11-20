45f057f5cb05952e0fee8415c2381d33d9a2f344
marekj <marekj.com@gmail.com>
2009-11-19 18:14:05 -0600

    complete about_control_statements with notes

--- koans/about_control_statements.rb
+++ koans/about_control_statements.rb
@@ -8,7 +8,7 @@ class AboutControlStatements < EdgeCase::Koan
     else
       result = :false_value
     end
-    assert_equal __, result
+    assert_equal :true_value, result
   end
 
   def test_if_then_else_statements
@@ -16,23 +16,23 @@ class AboutControlStatements < EdgeCase::Koan
     if true
       result = :true_value
     end
-    assert_equal __, result
+    assert_equal :true_value, result
   end
 
   def test_if_statements_return_values
     value = if true
-              :true_value
-            else
-              :false_value
-            end
-    assert_equal __, value
+      :true_value
+    else
+      :false_value
+    end
+    assert_equal :true_value, value
 
     value = if false
-              :true_value
-            else
-              :false_value
-            end
-    assert_equal __, value
+      :true_value
+    else
+      :false_value
+    end
+    assert_equal :false_value, value
 
     # NOTE: Actually, EVERY statement in Ruby will return a value, not
     # just if statements.
@@ -40,21 +40,22 @@ class AboutControlStatements < EdgeCase::Koan
 
   def test_if_statements_with_no_else_with_false_condition_return_value
     value = if false
-              :true_value
-            end
-    assert_equal __, value
+      :true_value
+    end
+    assert_equal nil, value #value is never init, by default all non init are nil
   end
 
   def test_condition_operators
-    assert_equal __, (true ? :true_value : :false_value)
-    assert_equal __, (false ? :true_value : :false_value)
+    assert_equal :true_value, (true ? :true_value : :false_value)
+    assert_equal :false_value, (false ? :true_value : :false_value)
+    # tricky part assert_equal "bus", ("car" ? "bus" : "car") # string is always true
   end
 
   def test_if_statement_modifiers
     result = :default_value
     result = :true_value if true
 
-    assert_equal __, result
+    assert_equal :true_value, result
   end
 
   def test_unless_statement
@@ -62,14 +63,14 @@ class AboutControlStatements < EdgeCase::Koan
     unless false
       result = :false_value
     end
-    assert_equal __, result
+    assert_equal :false_value, result
   end
 
   def test_unless_statement_modifier
     result = :default_value
     result = :false_value unless false
 
-    assert_equal __, result
+    assert_equal :false_value, result
   end
 
   def test_while_statement
@@ -79,7 +80,19 @@ class AboutControlStatements < EdgeCase::Koan
       result = result * i
       i += 1
     end
-    assert_equal __, result
+    assert_equal 3628800, result
+    # i result
+    # --------------
+    # 2 1 # after 1st call
+    # 3 2 # after 2nd call
+    # 4 6
+    # 5 24
+    # 6 120
+    # 7 720
+    # 8 5040
+    # 9 40320
+    # 10 362880
+    # 11 3628800 # exit trigger when i=11
   end
 
   def test_break_statement
@@ -90,7 +103,7 @@ class AboutControlStatements < EdgeCase::Koan
       result = result * i
       i += 1
     end
-    assert_equal __, result
+    assert_equal 3628800, result
   end
 
   def test_next_statement
@@ -98,10 +111,10 @@ class AboutControlStatements < EdgeCase::Koan
     result = []
     while i < 10
       i += 1
-      next if (i % 2) == 0
-      result << i 
+      next if (i % 2) == 0 #skip even numbers
+      result << i # collect odd numbers
     end
-    assert_equal __, result
+    assert_equal [1, 3, 5, 7, 9], result
   end
 
   def test_for_statement
@@ -110,7 +123,7 @@ class AboutControlStatements < EdgeCase::Koan
     for item in array
       result << item.upcase
     end
-    assert_equal [__, __, __], result
+    assert_equal ['FISH', 'AND', 'CHIPS'], result
   end
 
 end
