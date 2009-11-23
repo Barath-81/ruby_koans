e664378c475feeb1cec6be6dad25da0b97338586
marekj <marekj.com@gmail.com>
2009-11-23 16:52:07 -0600

    initial triangle method that passes tests

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,8 +14,26 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  case [a,b,c].uniq.size
+  when 1 then :equilateral
+  when 2 then :isosceles
+  when 3 then :scalene
+  end
 end
+
+# Example: bit ugly but follows a certain procedural train of thought
+# based on exhaustive comparison of each element to each element
+# :scalene is that side a is not the same as side b is not the same as side c
+# :equilateral if side a is equal to side b AND b to side c (therefore a == c)
+# def triangle2(a, b, c)
+#   if a!=b && b!=c && a!=c
+#     :scalene
+#   elsif a == b && b == c
+#     :equilateral
+#   else
+#     :isosceles #assume that two sides are equal
+#   end
+# end
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
